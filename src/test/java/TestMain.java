3e4e5bbe0586966a7610cd1c78da0d6a667f756b
Wazed-Matthis <39330800+Wazed-Matthis@users.noreply.github.com>
2020-06-26 13:24:16 +0200

    Update TestMain.java

--- src/test/java/TestMain.java
+++ src/test/java/TestMain.java
@@ -9,7 +9,7 @@ public class TestMain {
     public static void main(String[] args) {
         Api.init();
         try {
-            Account account = Generator.getInstance().generate("api-315a-db84-8d1a");
+            Account account = Generator.getInstance().generate("<api-key>");
             System.out.println(account.getUsername() + "\n" +  account.getPassword() + "\n" +  account.getToken());
             account.getInfo().forEach((e,e1)-> System.out.println(e + ": " + e1));
         } catch (UnauthorizedException e) {
